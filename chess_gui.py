9306e2d10aa2d88bef9cc4aa5f23e671bfc10bc6
jeed123456789 <51895465+jeed123456789@users.noreply.github.com>
2020-04-03 19:29:30 +0400

    Commented file

--- chess_gui.py
+++ chess_gui.py
@@ -1,3 +1,4 @@
+# import important modules to run this program: tkinter, chess, and PIL
 import tkinter
 from tkinter import PhotoImage
 import chess
@@ -5,11 +6,14 @@ from tkinter import messagebox
 from PIL import Image
 from PIL import ImageTk
 
-board = chess.Board()
-positions = []
-num_moves = 0
+board = chess.Board() # initialize chess board in the start of a game
+positions = [] # intialize list that will have all the buttons/positions pushed later used for making uci
+num_moves = 0 # initialize num_moves to keep track of how many moves were made in total both white and black
 
+# function to print the board, given the board_2d
+# go through the matrixed board then go through every single square in the board and update the chess piece image on every square.
 def print_board(board_2d, chess_frame, board_btn):
+    # dictionary to get the name of the chess piece's png file
     piece_img = {
         "b":"bb",
         "B":"bw",
@@ -24,45 +28,29 @@ def print_board(board_2d, chess_frame, board_btn):
         "r":"rb",
         "R":"rw"
         }
-    colored = ['b1','d1','f1','h1',
-               'a2','c2','e2','g2',
-               'b3','d3','f3','h3',
-               'a4','c4','e4','g4',
-               'b5','d5','f5','h5',
-               'a6','c6','e6','g6',
-               'b7','d7','f7','h7',
-               'a8','c8','e8','g8',]
-    piece_img_name = ''
-    piece = ''
+    piece_img_name = '' # create an empty string to create the file name of the chess piece_img
+    piece = '' # the name of the piece in matrized board ex) b, B, k, K, n, N, etc
+
+    # for loop to go through every single square in the board_2d and "print" the board
     for file in range(8):
         for rank in range(8):
-            chess_btn = board_btn[file][rank]
-            pos = chr(97+rank)+str(file+1)
-            if pos in colored:
-                if board_2d[file][rank] != '.':
-                    piece = board_2d[file][rank]
-                    piece_img_name = 'img_chess/'+piece_img[piece]+'.png'
-                    temp_img = Image.open(piece_img_name)
-                    temp_img = temp_img.resize((45,44), Image.LANCZOS)
-                    photoImg = ImageTk.PhotoImage(temp_img)
-                    chess_btn.image = photoImg
-                    chess_btn.configure(image = photoImg, height = 50, width = 46)
-                else:
-                    chess_btn.image = ''
-                    chess_btn.configure(image = '', height = 3, width = 6)
+            chess_btn = board_btn[file][rank]  # initialize chess_btn which is the button of a specific file and rank
+            if board_2d[file][rank] != '.': # if statement to check if there is a piece in the square, empty squares are '.'
+                piece = board_2d[file][rank]
+                piece_img_name = 'img_chess/'+piece_img[piece]+'.png'
+                # create temp_img to resize png file and display on the button
+                temp_img = Image.open(piece_img_name)
+                temp_img = temp_img.resize((45,44), Image.LANCZOS)
+                photoImg = ImageTk.PhotoImage(temp_img)
+                chess_btn.image = photoImg
+                chess_btn.configure(image = photoImg, height = 50, width = 46)
             else:
-                if board_2d[file][rank] != '.':
-                    piece = board_2d[file][rank]
-                    piece_img_name = 'img_chess/'+piece_img[piece]+'.png'
-                    temp_img = Image.open(piece_img_name)
-                    temp_img = temp_img.resize((45,45), Image.LANCZOS)
-                    photoImg = ImageTk.PhotoImage(temp_img)
+                # if there is no piece, get rid of the image on the button
+                chess_btn.image = ''
+                chess_btn.configure(image = '', height = 3, width = 6)
 
-                    chess_btn.image = photoImg
-                    chess_btn.configure(image = photoImg, height = 50, width = 46)
-                else:
-                    chess_btn.image = ''
-                    chess_btn.configure(image = '', height = 3, width = 6)
+# funciton to convert board to a matrix
+# convert board into a string then into a matrix
 def change_2d(board):
     ascii_board = str(board)
     board_2d = []
@@ -74,17 +62,25 @@ def change_2d(board):
             rank = []
     return board_2d
 
+# function for creating uci from buttons pushed and "pushing" move in board
 def make_move(positions, board):
-    global num_moves
-    
-    initial_pos = positions[num_moves-2]
-    final_pos = positions[num_moves-1]
-    uci =  initial_pos + final_pos
+    global num_moves # global variable to take care of unboundlocal error
+
+    initial_pos = positions[num_moves-2] # initialize initial_pos that will be connected to the "initial" button that has been pushed
+    final_pos = positions[num_moves-1] # initialize final_pos that will be connected to the "final" button that has been pushed
+    uci =  initial_pos + final_pos # uci is created by adding the starting square and the final squares
+
+    # if statement for checking if uci is an invalid uci
+    # if it is invalid, remove the two entries of buttons pushed and subtract 2 from num_moves
     if initial_pos == final_pos:
         positions.remove(initial_pos)
         positions.remove(final_pos)
         num_moves-=2
         return False
+
+    # if statement to check if the move can be made
+    # if it can be made, update the board and refresh the window
+    # if not, remove two entries of buttons pushed and subtract 2 from num_moves
     if not board.is_game_over():
         if not chess.Move.from_uci(uci) in board.legal_moves:
             positions.remove(initial_pos)
@@ -94,9 +90,9 @@ def make_move(positions, board):
         else:
             board.push_uci(uci)
             board_2d = change_2d(board)
-            print(board_2d)
             print_board(board_2d, chess_frame, board_btn)
-            
+
+            # check if there is stalemate, insufficient material, or checkmate in the board, if there any of those create messagebox and destroy window
             if board.is_stalemate():
                 tkinter.messagebox.showinfo(title = 'Game ended', message = 'Stalemate')
                 root.destroy()
@@ -104,42 +100,47 @@ def make_move(positions, board):
                 tkinter.messagebox.showinfo(title = 'Game ended', message = 'Insufficient material')
                 root.destroy()
             elif board.is_checkmate():
-                if chess.WHITE:
+                if chess.WHITE: # determine which side won or lost
                     tkinter.messagebox.showinfo(title = 'Game ended', message = 'Black lost. White won.')
                     root.destroy()
-                else:
+                else: # determine which side won or lost
                     tkinter.messagebox.showinfo(titel = 'Game ended', message = 'White lost. Black won.')
                     root.destroy()
-            elif board.is_check():
+            elif board.is_check(): # check if there is a check
                 tkinter.messagebox.showinfo(title = 'Check', message = 'Check')
             return True
 
-    
-    
+
+# function that will be called when buttons are pushed, increments num_moves and adds the name of the button id in positions
 def tell_position(btn_id):
     global num_moves
     positions.append(btn_id)
     num_moves+=1
-    if num_moves%2 == 0 and num_moves != 0:
-        if not make_move(positions, board):
+    if num_moves%2 == 0 and num_moves != 0: # if statement for making move every time num_moves is divisible by 2.
+        if not make_move(positions, board): # if make_move returns False, an illegal move was made, messagebox that talks about illegal move is posted.
             tkinter.messagebox.showinfo(title = 'Illegal move', message = 'That is an illegal move')
 
+# function for when reset_game is pressed from the menu bar
 def reset_game(board):
     board.reset()
     board_2d = change_2d(board)
     print_board(board_2d, chess_frame, board_btn)
-    
+
+# function for when help is pressed from the menu bar
+def offer_help():
+    tkinter.messagebox.showinfo(title = 'Help', message = 'No help can be given in a fair game of chess')
+
+# create root window for program, set title and size
 root = tkinter.Tk()
 root.title("Chess Program")
 root.geometry("420x450")
 
+# create frame for where the board will stay in
+chess_frame = tkinter.Frame(root, height = 500, width = 420, relief = 'sunken', bd = 2, bg = '#D9E5FF')
+#chess_frame.pack(side = 'left', expand = True, fill = 'both')
 
-
-chess_frame = tkinter.Frame(root, height = 500, width = 500, relief = 'sunken', bd = 2, bg = '#D9E5FF')
-chess_frame.pack(side = 'left', expand = True, fill = 'both')
-
-
-
+# create button for each square in the board, the background, width, height, and bide command each button to tell_position
+# then place the button in appropiate grid to resemble a chess board
 a8 = tkinter.Button(chess_frame, bg = 'white', width = 6, height = 3, command = lambda:tell_position('a8'))
 a8.grid(row = 0, column = 0)
 b8 = tkinter.Button(chess_frame, bg = 'green', width = 6, height = 3, command = lambda:tell_position('b8'))
@@ -276,6 +277,7 @@ g1.grid(row = 7, column = 6)
 h1 = tkinter.Button(chess_frame, bg = 'white', width = 6, height = 3, command = lambda:tell_position('h1'))
 h1.grid(row = 7, column = 7)
 
+# create board_btn, a matrix that can reference to every button/square in the board
 board_btn = [[a8, b8, c8, d8, e8, f8, g8, h8],
                 [a7, b7, c7, d7, e7, f7, g7, h7],
                 [a6, b6, c6, d6, e6, f6, g6, h6],
@@ -285,12 +287,11 @@ board_btn = [[a8, b8, c8, d8, e8, f8, g8, h8],
                 [a2, b2, c2, d2, e2, f2, g2, h2],
                 [a1, b1, c1, d1, e1, f1, g1, h1]]
 
-
-
-
+# initialize the board for the first time and "print" board
 board_2d = change_2d(board)
-print_board(board_2d, chess_frame,board_btn)  
+print_board(board_2d, chess_frame,board_btn)
 
+# create menubar and bind commands to them
 menubar = tkinter.Menu(root)
 root['menu'] = menubar
 menu_cmd = tkinter.Menu(menubar, tearoff = 0)
@@ -299,12 +300,6 @@ menubar.add_cascade(menu = menu_cmd, label = 'Commands')
 menubar.add_cascade(menu = menu_help, label = 'Help')
 menu_cmd.add_command(label = 'Quit', command =root.destroy)
 menu_cmd.add_command(label = 'Reset Game', command = lambda:reset_game(board))
-
-  
+menu_help.add_command(label = 'Help', command = offer_help)
 
 root.mainloop()
-
-
-
-
-
